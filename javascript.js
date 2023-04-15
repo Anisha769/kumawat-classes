64646191b5f39fd72d98872a8666b84ee0442995
Anisha769 <anishaprajapati769@gmail.com>
2023-04-15 18:42:52 +0530

    new changes

--- javascript.js
+++ javascript.js
@@ -1,41 +1,55 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js'
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
+console.log('hellooooo')
 const firebaseConfig = {
-    apiKey: "AIzaSyDLmmMrGCu7qoiU6qt0HlAWqsiyVhIzM-M",
-    authDomain: "contactform-ddbed.firebaseapp.com",
-    databaseURL: "https://contactform-ddbed-default-rtdb.firebaseio.com",
-    projectId: "contactform-ddbed",
-    storageBucket: "contactform-ddbed.appspot.com",
-    messagingSenderId: "1006605943914",
-    appId: "1:1006605943914:web:3c394e12d2afc13b623613"
-  };
+  apiKey: "AIzaSyDLmmMrGCu7qoiU6qt0HlAWqsiyVhIzM-M",
+  authDomain: "contactform-ddbed.firebaseapp.com",
+  databaseURL: "https://contactform-ddbed-default-rtdb.firebaseio.com",
+  projectId: "contactform-ddbed",
+  storageBucket: "contactform-ddbed.appspot.com",
+  messagingSenderId: "1006605943914",
+  appId: "1:1006605943914:web:3c394e12d2afc13b623613"
+};
 // initialize firebase
-  firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log('asdasdsd')
+
 
 //   reference your database
-const contactFormDB = firebase.database().ref("contactForm");
+// const contactFormDB = firebase.database().ref("contactForm");
 document.getElementById('contacting').addEventListener("submit", submitForm);
 
-function submitForm(e){
-    e.preventDefault();
+function submitForm(e) {
+  console.log('avinash')
+  e.preventDefault();
 
-    var name = getElementVal('name');
-    var number = getElementVal('number');
-    var email = getElementVal('email');
-    var subject = getElementVal('subject');
-    var message = getElementVal('message');
+  var name = document.getElementById('name').value;
+  var number = document.getElementById('number').value;
+  var email = document.getElementById('email').value;
+  var subject = document.getElementById('subject').value;
+  var message = document.getElementById('message').value;
 
-    saveMessages(name,number,email,subject,message);
+  saveMessages(name, number, email, subject, message);
 }
 
 
 
-const saveMessages = (name,number,email,subject,message) =>{
-    var newContactForm = contactFormDB.push();
-
-    newContactForm.set({
-        name: name,
-        number: number,
-        email: email,
-        subject: subject,
-        message: message,
-    });
+const saveMessages = async (name, number, email, subject, message) => {
+  console.log("EHHEHHEE",
+    name,
+    number,
+    email,
+    subject,
+    message)
+  const docRef = await addDoc(collection(db, "contact"), {
+    name,
+    number,
+    email,
+    subject,
+    message
+  });
+  console.log("Document written with ID: ", docRef.id);
 }
